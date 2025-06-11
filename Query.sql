9f43bd93d10f013d7175ed5ccab4b371f13c2383
Sahil Sayyad <96936602+SahilSayyad@users.noreply.github.com>
2025-06-11 13:51:08 +0530

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -15,4 +15,70 @@ SET Item_Fat_Content =
 
 SELECT DISTINCT Item_Fat_Content FROM blinkit_data;
 
+/*Total Sales: The overall revenue generated from all items sold.*/
+SELECT CAST(SUM(Total_Sales) / 1000000.0 AS DECIMAL(10,2)) AS Total_Sales_Million
+FROM blinkit_data;
+
+/*Average Sales: The average revenue per sale.*/
+SELECT CAST(AVG(Total_Sales) AS INT) AS Avg_Sales
+FROM blinkit_data;
+
+/*Number of Items: The total count of different items sold.*/
+SELECT COUNT(*) AS No_of_Orders
+FROM blinkit_data;
+
+/*Average Rating: The average customer rating for items sold.*/
+SELECT CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating
+FROM blinkit_data;
+
+/*Total Sales by Fat Content*/
+SELECT Item_Fat_Content, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Item_Fat_Content;
+
+/*Fat Content by Outlet for Total Sales*/
+SELECT Outlet_Location_Type, 
+       ISNULL([Low Fat], 0) AS Low_Fat, 
+       ISNULL([Regular], 0) AS Regular
+FROM 
+(
+    SELECT Outlet_Location_Type, Item_Fat_Content, 
+           CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+    FROM blinkit_data
+    GROUP BY Outlet_Location_Type, Item_Fat_Content
+) AS SourceTable
+PIVOT 
+(
+    SUM(Total_Sales) 
+    FOR Item_Fat_Content IN ([Low Fat], [Regular])
+) AS PivotTable
+ORDER BY Outlet_Location_Type;
+
+/*Total Sales by Outlet Establishment*/
+SELECT Outlet_Establishment_Year, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Outlet_Establishment_Year
+ORDER BY Outlet_Establishment_Year;
+
+/*Percentage of Sales by Outlet Size*/
+SELECT 
+    Outlet_Size, 
+    CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+    CAST((SUM(Total_Sales) * 100.0 / SUM(SUM(Total_Sales)) OVER()) AS DECIMAL(10,2)) AS Sales_Percentage
+FROM blinkit_data
+GROUP BY Outlet_Size
+ORDER BY Total_Sales DESC;
+
+/*All Metrics by Outlet Type*/
+SELECT Outlet_Type, 
+CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+		CAST(AVG(Total_Sales) AS DECIMAL(10,0)) AS Avg_Sales,
+		COUNT(*) AS No_Of_Items,
+		CAST(AVG(Rating) AS DECIMAL(10,2)) AS Avg_Rating,
+		CAST(AVG(Item_Visibility) AS DECIMAL(10,2)) AS Item_Visibility
+FROM blinkit_data
+GROUP BY Outlet_Type
+ORDER BY Total_Sales DESC;
+
+
 
